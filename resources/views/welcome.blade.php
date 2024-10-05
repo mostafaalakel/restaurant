dbd949fe4b57faa5559d4b81736039518ce2720f
mostafa akel <mostafaalakel947@gmail.com>
2024-10-05 14:37:22 +0300

    edit title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Restaurant</title>
+        <title>My Restaurant</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
