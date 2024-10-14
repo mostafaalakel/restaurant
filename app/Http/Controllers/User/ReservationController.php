496776ea5f60a45fa3a3634f030f90390333b59a
mostafa akel <mostafaalakel947@gmail.com>
2024-10-14 10:55:58 +0300

    init commit

--- app/Http/Controllers/User/ReservationController.php
+++ app/Http/Controllers/User/ReservationController.php
@@ -14,78 +14,17 @@ use App\Http\Resources\ReservationResource;
 class ReservationController extends Controller
 {
   use ApiResponseTrait;
-  // public function createReservation(Request $request)
-  // {
-  //   $rules = [
-  //     'num_people' => 'required',
-  //     'reservation_time' => 'required|date_format:Y-m-d H:i:s|after_or_equal:now',
-  //     'special_request' => 'required'
-  //   ];
-  //   $validate = Validator::make($request->all(), $rules);
-
-  //   if ($validate->fails()) {
-  //     return response()->json(['status' => 'validation_error', 'errors' => $validate->errors()]);
-  //   }
-
-  //   $createReservation = Reservation::create([
-  //     'user_id' => Auth::id(),
-  //     'num_people' => $request->num_people,
-  //     'reservation_time' => $request->reservation_time,
-  //     'special_request' => $request->special_request
-  //   ]);
-
-  //   if ($createReservation) {
-  //     return $this->apiResponse(201, 'Reservation done Successfully');
-  //   } else {
-  //     return $this->apiResponse(404, 'Reservation failed');
-  //   }
-  // }
-
-  // public function showReservation($user_id)
-  // {
-  //   $userReservation = User::find($user_id)->reservations;
-  //   if (!$userReservation->isEmpty()) {
-  //     $userReservation = ReservationResource::collection($userReservation);
-  //     return $this->apiResponse(200, 'your Reservation got it Successfully', $userReservation);
-  //   } else {
-  //     return $this->apiResponse(404, 'you don`t have any Reservation');
-  //   }
-  // }
-
-  // public function deleteReservation($id)
-  // {
-  //   $reservation = Reservation::findOrFail($id);
-  //   $reservation->delete();
-  //   return $this->apiResponse('200', message: 'reservation deleted successfully');
-  // }
-
-  // public function updateReservation(Request $request, $id)
-  // {
-
-  //   $rules = [
-  //     'num_people' => 'required',
-  //     'reservation_time' => 'required|date_format:Y-m-d H:i:s|after_or_equal:now',
-  //     'special_request' => 'required'
-  //   ];
-  //   $validate = Validator::make($request->all(), $rules);
-
-  //   if ($validate->fails()) {
-  //     return response()->json(['status' => 'validation_error', 'errors' => $validate->errors()]);
-  //   }
-
-  //   $reservation = Reservation::findOrFail($id);
-  //   $reservation->update([
-  //     'user_id' => Auth::id(),
-  //     'num_people' => $request->num_people,
-  //     'reservation_time' => $request->reservation_time,
-  //     'special_request' => $request->special_request
-  //   ]);
-
-  //   return $this->apiResponse('200', 'reservation updated successfully');
-  // }
-
   public function createReservation(Request $request)
   {
+
+    $existingReservation = Reservation::where('user_id', Auth::id())
+      ->where('status', 'processing')
+      ->first();
+
+    if ($existingReservation) {
+      $existingReservation->delete();
+    }
+
     $rules = [
       'num_people' => 'required|integer|min:1',
       'reservation_time' => 'required|date_format:Y-m-d H:i:s|after_or_equal:now',
@@ -113,7 +52,7 @@ class ReservationController extends Controller
 
   public function showReservation($user_id)
   {
-    $userReservation = User::find($user_id)->reservations;
+    $userReservation = User::find($user_id)->reservations()->orderByDesc('id')->get();
 
     if (!$userReservation->isEmpty()) {
       $userReservationResource = ReservationResource::collection($userReservation);
--- app/Http/Controllers/User/ReviewController.php
+++ app/Http/Controllers/User/ReviewController.php
@@ -13,43 +13,16 @@ use Illuminate\Support\Facades\Validator;
 class ReviewController extends Controller
 {
     use ApiResponseTrait;
-    // public function AddReviews(Request $request)
-    // {
-    //     $rules = [
-    //         'food_id' => 'required|exists:foods,id',
-    //         'rating' => 'required|integer|min:1|max:5',
-    //         'comment' => 'nullable|string',
-    //     ];
-    //     $validate = Validator::make($request->all(), rules: $rules);
-    //     if ($validate->fails()) {
-    //         return response()->json([
-    //             'status' => 'validation_error',
-    //             'errors' => $validate->errors()
-    //         ]);
-    //     }
-    //     $review = Review::create([
-    //         'user_id' => Auth::id(),
-    //         'food_id' => $request->food_id,
-    //         'rating' => $request->rating,
-    //         'comment' => $request->comment,
-    //     ]);
-
-    //     return $this->apiResponse(201, 'Review added successfully');
-    // }
-
-    // public function showReviews($food_id)
-    // {
-    //     $reviews = Review::where('food_id', $food_id)->with('user')->get();
-    //     if (!$reviews->isEmpty()) {
-    //         $reviews = ReviewResource::collection($reviews);
-    //         return $this->apiResponse(200, 'Reviews are got it successfully', $reviews);
-    //     } else
-    //         return $this->apiResponse(200, 'we haven`t any reviews yet ', $reviews);
-    // }
-
-
     public function addReviews(Request $request)
     {
+        $existingReview = Review::where('food_id', $request->food_id)
+            ->where('user_id', Auth::id())
+            ->first();
+
+        if ($existingReview) {
+            $existingReview->delete();
+        }
+
         $rules = [
             'food_id' => 'required|exists:foods,id',
             'rating' => 'required|integer|min:1|max:5',
