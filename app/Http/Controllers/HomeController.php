5a85080885fb400b5cda122a0fab93fdc3e8f410
mostafaalakel <mostafaalakel947@gmail.com>
2025-01-30 12:13:06 +0300

    fix bug on HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,29 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\User\FoodController;
-use App\Http\Resources\FoodResource;
 use App\Http\Resources\FoodSummaryResource;
 use App\Http\Traits\ApiResponseTrait;
-use App\Models\category;
 use App\Models\Food;
-use Illuminate\Http\Request;
+use App\Services\User\FoodService;
 
-class HomeController extends FoodController
+class HomeController extends Controller
 {
     use ApiResponseTrait;
+
+    protected $foodService;
+
+    public function __construct(FoodService $foodService)
+    {
+        $this->foodService = $foodService;
+    }
 
     public function index()
     {
         // sorting data depends on rating and discounts value (FoodGeneralDiscounts and withAverageRating are query scope in food model)
         $foods = Food::FoodGeneralDiscounts()->withAverageRating()->get();
-
         $foods->transform(function ($food) {
-            return $this->checkIfFoodHasDiscountAndGetPriceAfterDiscounts($food);
+            return $this->foodService->applyDiscountIfAvailable($food);
         });
-
         return $this->retrievedResponse(FoodSummaryResource::collection($foods), 'Food discount retrieved successfully');
-
-
     }
 }
